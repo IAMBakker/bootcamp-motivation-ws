04d2464d7600c15b94949a94b46179ddc654cf6e
IAMBakker <icobakker@valori.nl>
2020-06-08 14:22:22 +0200

    Added dockerifle and updated exposed endpoints to rest resources

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
 from flask import Flask
 from flask import jsonify
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-@app.route("/welcome/")
-def welcome():
-    message = "Welkom bij de bootcamp toelatings opdracht. Werk alsjeblieft zorgvuldig en netjes. Succes!!"
-    return jsonify(
-        author="unknown",
-        message=message
-    )
+class Welcome(Resource):
+    def get(self):
+        return {'welcome': 'Welkom bij de bootcamp toelatings opdracht. Werk alsjeblieft zorgvuldig en netjes. Succes!!'}
+        
+class Motivation(Resource):
+    def get(self):
+        message = """Na het volgen van de bootcamp hoop ik: Een beter beeld te hebben van best practices op het gebied van 
+        testautomatisering; Te leren van inzichten en perspectieven van mede-bootcampers; Uitgedaagd te worden om een nieuwe
+        test tool en programmeertaal te proberen; Mijn portfolio met test automation code met een flink aantal scripts uit
+        te breiden; Beter te begrijpen hoe je test automation aan pakt om zinnige en stabiele testcases te maken, en 
+        last but certainly not least: Het lijkt me gewoon heel erg vet om aan de bootcamp mee te doen!!
+            """
 
-@app.route("/motivation/")
-def motivation():
-    message = """Na het volgen van de bootcamp hoop ik: Een beter beeld te hebben van best practices op het gebied van 
-testautomatisering; Te leren van inzichten en perspectieven van mede-bootcampers; Uitgedaagd te worden om een nieuwe
-test tool en programmeertaal te proberen; Mijn portfolio met test automation code met een flink aantal scripts uit
-te breiden; Beter te begrijpen hoe je test automation aan pakt om zinnige en stabiele testcases te maken, en 
-last but certainly not least: Het lijkt me gewoon heel erg vet om aan de bootcamp mee te doen!!
-    """
+        return {'hello': 'world',
+            'message': message}
 
-    return jsonify(
-        author="Ico Bakker",
-        message=message
-    )
-
+api.add_resource(HelloWorld, '/')
+api.add_resource(Welcome, '/welcome/')
+api.add_resource(Motivation, '/motivation/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='5000')
